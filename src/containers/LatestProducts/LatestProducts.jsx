22bea00592f84e6f307e45178a130ebebfeed33f
Boris Tenkes <boris.tenkes@gmail.com>
2023-03-12 17:54:59 +0100

    Compressed new product images

--- src/containers/LatestProducts/LatestProducts.jsx
+++ src/containers/LatestProducts/LatestProducts.jsx
@@ -5,7 +5,9 @@ import './LatestProducts.scss';
 export default function LatestProducts() {
 	const [products, fetchError, isLoading] = useFetch('/products');
 
-	const latestProducts = products.slice(0, 8);
+	const latestProducts = products
+		.sort((p1, p2) => (Date.parse(p1.date) > Date.parse(p2.date) ? -1 : 1))
+		.slice(0, 8);
 
 	return (
 		<section className='container section-padding'>
